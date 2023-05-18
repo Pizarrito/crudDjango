ed57daa5115500f3b4456d4130c29d8082ca2805
Pizarrito <102396556+Pizarrito@users.noreply.github.com>
2023-05-18 10:09:17 -0400

    Arreglo models

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # crudDjango
+
+# buscar como importar el archivo csv 
+
+# listar los datos importados filtrando la fecha y el sabor 
+
+#
--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+#crear tabla ventas
+class ventas(models.Model):
+    fecha_venta = models.DateField(_("fecha_venta"), auto_now=True)
+    sabor = models.CharField(_("sabor"), max_length = 255)
+    cantidad = models.IntegerField(_("cantidad"))
+    gramos = models.IntegerField(_("gramos"))
