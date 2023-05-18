79b10bfabe3fccc6acb43bfc48bd1dc1ec385c5d
Pizarrito <102396556+Pizarrito@users.noreply.github.com>
2023-05-18 19:35:42 -0400

    sumas y tablas

--- vista/views.py
+++ vista/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.db.models import Sum
 from .models import ventas
 
 # Create your views here.
 
 def list(request):
     datosListado = ventas.objects.all()
-    return render(request,'datos.html',{"ventas":datosListado})
+    resultado = ventas.objects.aggregate(total_cantidad=Sum("cantidad"))
+    resultado2 = ventas.objects.aggregate(total_gramos=Sum("gramos_unidad"))
+    
+    return render(request,'datos.html',{"ventas":datosListado,"resultado":resultado,"resultado2":resultado2})
+
