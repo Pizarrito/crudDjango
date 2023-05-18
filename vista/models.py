41f9e4d8bfe1fda6da89c3c0fa19ba1f975426f5
Pizarrito <102396556+Pizarrito@users.noreply.github.com>
2023-05-18 18:51:46 -0400

    Tabla-Templates

--- vista/models.py
+++ vista/models.py
@@ -4,6 +4,7 @@ from django.utils.translation import gettext as _
 # Create your models here.
 
 class ventas (models.Model):
+    id = models.AutoField(_('id'), primary_key=True)
     fecha_venta = models.DateField(_("fecha_venta"),auto_now = True)
     sabor_helado = models.CharField(_("sabor_helado"),max_length=255)
     cantidad = models.IntegerField(_("cantidad"))
--- vista/views.py
+++ vista/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
-
+from .models import ventas
 
 # Create your views here.
+
+def list(request):
+    datosListado = ventas.objects.all()
+    return render(request,'datos.html',{"ventas":datosListado})
