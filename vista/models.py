7b734e076671662cdfa5fd4344342d510ff5cb3b
Pizarrito <102396556+Pizarrito@users.noreply.github.com>
2023-05-18 15:39:01 -0400

    Arreglo pgAdmin

--- vista/models.py
+++ vista/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 # Create your models here.
+
+class ventas (models.Model):
+    fecha_venta = models.DateField(_("fecha_venta"),auto_now = True)
+    sabor_helado = models.CharField(_("sabor_helado"),max_length=255)
+    cantidad = models.IntegerField(_("cantidad"))
+    gramos_unidad = models.IntegerField(_("gramos_unidad"))
